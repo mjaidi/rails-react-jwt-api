1e63beb3d5f79b9ca451983027e9b8ff4f3d36fe
Majid Jaidi <majid.jaidi@gmail.com>
2019-09-24 17:22:33 +0100

    cra backend api

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  devise_for :users,
+  path: '',
+  path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'sessions',
+    registrations: 'registrations'
+  }
+
+  namespace :api do
+    namespace :v1 do
+      resources :articles
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
