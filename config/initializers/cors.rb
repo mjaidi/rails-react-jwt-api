d4590179f49cc8d1c1ab4f7fb09c5c34f809fb57
Majid Jaidi <majid.jaidi@gmail.com>
2019-09-24 17:31:21 +0100

    updated coors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://inspiring-nobel-089436.netlify.com'
+    origins 'http://localhost:3000', 'https://inspiring-nobel-089436.netlify.com'
 
     resource '*',
       headers: :any,
